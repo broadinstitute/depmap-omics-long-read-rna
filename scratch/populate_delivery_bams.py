68f7cc4431dc47fea4153b5060bc315c54fcd633
Devin McCabe <dmccabe@broadinstitute.org>
2024-10-10 15:49:16 -0400

    better initial delivery_bam population

--- scratch/populate_delivery_bams.py
+++ scratch/populate_delivery_bams.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from nebelung.terra_workspace import TerraWorkspace
 
 from dogspa_long_reads.types import GumboClient, ModelsAndChildren
+from dogspa_long_reads.utils.bams import assign_cds_ids
 from dogspa_long_reads.utils.onboarding import explode_and_expand_models
 from dogspa_long_reads.utils.utils import model_to_df
 
@@ -13,8 +14,6 @@ load_dotenv()
 terra_workspace = TerraWorkspace(
     workspace_namespace="broad-firecloud-ccle", workspace_name="Long_Read_Omics"
 )
-
-terra_samples = terra_workspace.get_entities("sample")
 
 pilot = pd.read_csv("./data/pilot.csv")
 pb_pre = pd.read_csv("./data/pb_pre.csv")
@@ -109,15 +108,8 @@ delivery_bams = pd.concat(
 
 assert ~delivery_bams["profile_id"].duplicated().any()
 
-delivery_bams = delivery_bams.merge(
-    terra_samples[["sample_id", "LongReadProfileID"]].rename(
-        columns={"LongReadProfileID": "profile_id"}
-    ),
-    how="left",
-    on="profile_id",
-)
-
-assert delivery_bams["sample_id"].notna().all()
+delivery_bams["delivery_bam_id"] = delivery_bams["delivery_bam"].apply(os.path.basename)
+delivery_bams = assign_cds_ids(delivery_bams, "00000000-0000-0000-0000-000000000000")
 
 aligned_bams = (
     pd.read_csv("./data/legacy_aligned_bams.tsv", sep="\t")
@@ -127,11 +119,60 @@ aligned_bams = (
     )
 )
 
+last_batch = {
+    "ACH-000040",
+    "ACH-000212",
+    "ACH-000246",
+    "ACH-000261",
+    "ACH-000280",
+    "ACH-000288",
+    "ACH-000455",
+    "ACH-000622",
+    "ACH-000634",
+    "ACH-000783",
+    "ACH-000906",
+    "ACH-000913",
+    "ACH-000946",
+    "ACH-001067",
+    "ACH-001329",
+    "ACH-001386",
+    "ACH-001415",
+    "ACH-001538",
+    "ACH-001606",
+    "ACH-001607",
+    "ACH-001608",
+    "ACH-001609",
+    "ACH-001611",
+    "ACH-001619",
+    "ACH-001622",
+    "ACH-001623",
+    "ACH-001652",
+    "ACH-001673",
+    "ACH-001687",
+    "ACH-001843",
+    "ACH-001844",
+    "ACH-001850",
+    "ACH-001853",
+    "ACH-001960",
+    "ACH-001961",
+    "ACH-002070",
+    "ACH-002533",
+    "ACH-002680",
+    "ACH-002681",
+    "ACH-003130",
+    "ACH-003193",
+    "ACH-003233",
+    "ACH-003235",
+    "ACH-003237",
+    "ACH-003337",
+}
+
+aligned_bams = aligned_bams.loc[~aligned_bams["model_id"].isin(last_batch)]
+
 delivery_bams = delivery_bams.merge(aligned_bams, how="left", on="model_id")
 
 assert ~delivery_bams["model_id"].duplicated().any()
 
-delivery_bams["delivery_bam_id"] = delivery_bams["delivery_bam"].apply(os.path.basename)
 bam_ids = delivery_bams.pop("delivery_bam_id")
 delivery_bams.insert(0, "entity:delivery_bam_id", bam_ids)
 
